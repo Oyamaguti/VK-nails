e39e1850a707b7db7a58ac59c6182de8e70f14a0
Oyamaguti <vichavesoyamaguti@gmail.com>
2025-01-24 17:39:34 -0300

    finalização do site com a adição do menu e a inserção das características de cada esmalte

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu-vertical');
+    const menuToggle = document.querySelector('.menu-toggle')
+    const menu = document.querySelector('.menu-vertical')
 
-    menuToggle.style.left = '0px';
+    menuToggle.style.left = '0px'
 
     menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('open');
+        menu.classList.toggle('open')
 
         if (menu.classList.contains('open')) {
             setTimeout(() => {
-                menuToggle.style.left = '250px';
-            }, 50);
+                menuToggle.style.left = '250px'
+            }, 50)
         } else {
-            menuToggle.style.left = '0px';
+            menuToggle.style.left = '0px'
         }
-    });
-});
+    })
+})
